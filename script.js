4948681ea09f4c085d3988b43cb5ea0b9c8b8955
Sophia De La Rosa <sophial.delarosa@gmail.com>
2022-05-12 20:16:04 -0400

    Version 2: Working prompt and confirms for all types.

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
-// Assignment Code
+// Variables
 var generateBtn = document.querySelector("#generate");
 
-var passLength = prompt("How many characters should your password be?", "Please choose between 8 and 128 characters.");
 
-if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
-  alert("Please choose a length between 8 and 128 characters");
-} 
+function generatePassword() {
+  var passLength = prompt("How many characters should your password be?", "Please choose between 8 and 128 characters.");
+  while (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+    passLength = prompt("Please choose a length between 8 and 128 characters.");
+  }
+  //variable for lowercase
+  var passLower = confirm("Would you like to include lowercase letters?"); 
+  //variable for uppercase
+  var passUpper = confirm("Would you like to include uppercase letters?");
+  //variable for numbers
+  var passNumbers = confirm("Would you like to include numbers?");
+  //variable for special characters
+  var passSpecial = confirm("Would you like to include special characters?");
+}
 
 
 
-// Write password to the #password input
-function writePassword() {
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
+
+
+
+generateBtn.addEventListener("click", generatePassword);
+
+
+
+
+//Write password to the #password input
+//function writePassword() {
+   
   
 //var password = generatePassword();
 //var passwordText = document.querySelector("#password");
@@ -26,8 +46,7 @@ function writePassword() {
 //passwordText.value = password;
 
 
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
 
 //if (promptLength < 8 || promptLength > 128 || isNaN(promptLength)) {
